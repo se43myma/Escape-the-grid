47e769403ced5228f86c8a637250266b42848027
se43myma <praneeth.nallan@fau.de>
2021-12-16 13:54:32 +0100

    location change

---
+++ build/CMakeFiles/gridtest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gridtest.dir/MyGrid.cpp.o"
+  "CMakeFiles/gridtest.dir/Tile.cpp.o"
+  "CMakeFiles/gridtest.dir/gridtest.cpp.o"
+  "gridtest"
+  "gridtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gridtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
